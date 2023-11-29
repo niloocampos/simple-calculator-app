e1d31c62ddd5aeae5bfaa95c81484f8a9656ec4b
niloocampos <nvancity@gmail.com>
2023-11-28 21:11:26 -0800

    Added all helper functions for each button and added event listeners

--- src/App.js
+++ src/App.js
@@ -3,21 +3,55 @@ import './App.css';
 
 function App() {
   const [result, setResult] = useState(0);
-  const resultRef = useRef();
-  const inputRef = useRef();
+  const resultRef = useRef(null);
+  const inputRef = useRef(null);
+
+  const plus = (e) => {
+    e.preventDefault();
+    setResult(result + Number(inputRef.current.value));
+  };
+
+  const minus =(e) => {
+    e.preventDefault();
+    setResult(result - Number(inputRef.current.value));
+  };
+
+  const times =(e) => {
+    e.preventDefault();
+    setResult(result * Number(inputRef.current.value));
+  };
+
+  const divide =(e) => {
+    e.preventDefault();
+    setResult(result / Number(inputRef.current.value));
+  };
+
+  const resetInput = (e) => {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  };
+
+  const resetResult = (e) => {
+    e.preventDefault();
+    setResult(result => result * 0);
+  } 
+
+
+
+
   return (
     <div className="App">
       <h1>Simple Calculator</h1>
-      <h3>Result: 0</h3>
+      <h3 ref={resultRef}>Result: {result}</h3>
       <form>
         <label>Input</label>
-        <input placholder="Type a number" type="number" pattern="[0-9]"  />
-        <button>Add</button>
-      <button>Subtract</button>
-      <button>Multiply</button>
-      <button>Divide</button>
-      <button>Reset Input</button>
-      <button>Reset Result</button>
+        <input placholder="Type a number" type="number" pattern="[0-9]" ref={inputRef}  />
+        <button onClick={plus}>Add</button>
+      <button onClick={minus}>Subtract</button>
+      <button onClick={times}>Multiply</button>
+      <button onClick={divide}>Divide</button>
+      <button onClick={resetInput}>Reset Input</button>
+      <button onClick={resetResult}>Reset Result</button>
       </form>
       
       
