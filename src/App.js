66e06f9b7595cf4d0261181f09ef270d38fbeb52
niloocampos <nvancity@gmail.com>
2023-11-28 20:33:17 -0800

    Created main structure of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Calculator</h1>
+      <h3>Result: 0</h3>
+      <button>Add</button>
+      <button>Subtract</button>
+      <button>Multiply</button>
+      <button>Divide</button>
+      <button>Reset Input</button>
+      <button>Result Result</button>
+      
     </div>
   );
 }
