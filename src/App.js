d5d230a1054c0d5b810590657024b9f0a9067095
niloocampos <nvancity@gmail.com>
2023-11-28 20:57:28 -0800

    Added hooks useRef and useState and relevant variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState(0);
+  const resultRef = useRef();
+  const inputRef = useRef();
   return (
     <div className="App">
       <h1>Simple Calculator</h1>
       <h3>Result: 0</h3>
-      <button>Add</button>
+      <form>
+        <label>Input</label>
+        <input placholder="Type a number" type="number" pattern="[0-9]"  />
+        <button>Add</button>
       <button>Subtract</button>
       <button>Multiply</button>
       <button>Divide</button>
       <button>Reset Input</button>
-      <button>Result Result</button>
+      <button>Reset Result</button>
+      </form>
+      
       
     </div>
   );
